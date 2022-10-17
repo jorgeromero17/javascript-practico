1ab09e60b959bf36156bb963bf092b4acedfe90f
jorgeromero17 <jorgeromero98@outlook.com>
2022-10-16 19:13:43 -0600

    carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const productDetail = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click',toggleMobileMenu);
+shoppingCartIcon.addEventListener('click',toggleShoppingCart);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+  productDetail.classList.add('inactive');
+  desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
+  productDetail.classList.add('inactive');
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  productDetail.classList.toggle('inactive');
 }
