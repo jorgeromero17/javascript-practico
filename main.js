b0f7175ba6945e53946ccb82e4093d4248a6056f
jorgeromero17 <jorgeromero98@outlook.com>
2022-10-17 00:19:22 -0600

    agregando dinamismo al product detail

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ const cardsContainer = document.querySelector('.cards-container');
 const productDetailContainer = document.querySelector("#productDetail");
 const productDetailCloseIcon = document.querySelector('.product-detail-close');
 productDetailCloseIcon.addEventListener('click',closedProductDetailAside);
+//aside de detalles elementos a reemplazar
+const productImageDetail = document.querySelector('#productImageDetail');
+const productPriceDetail = document.querySelector('#productPriceDetail');
+const productNameDetail = document.querySelector('#productNameDetail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click',toggleMobileMenu);
@@ -36,41 +40,46 @@ function toggleShoppingCart() {
   shoppingCartContainer.classList.toggle('inactive');
 }
 
-function openProductDetailAside() {
-  shoppingCartContainer.classList.add('inactive');
-  mobileMenu.classList.add('inactive');
-  desktopMenu.classList.add('inactive');
-  productDetailContainer.classList.remove('inactive');
-  
-}
-
 function closedProductDetailAside() {
   productDetailContainer.classList.add('inactive');
 }
+
+
 //Productos
 const productList = [];
 productList.push(
 {
+  id:0,
   name:'Bike',
   price: 120,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 },
 {
+  id:1,
   name: 'Pro Camera',
   price: 1200,
   image:'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 },
 {
+  id:2,
   name: 'Fragance',
   price:50,
   image: 'https://images.pexels.com/photos/12538232/pexels-photo-12538232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 },
 {
+  id:3,
   name: `Men's Watch`,
   price:100,
   image: 'https://images.pexels.com/photos/9982635/pexels-photo-9982635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 },
 );
+
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
 
 function renderProducts(product) {
   for (const product of productList) {
@@ -79,7 +88,12 @@ function renderProducts(product) {
   
     const productImage = document.createElement('img');
     productImage.setAttribute('src',product.image);
-    productImage.addEventListener('click',openProductDetailAside)
+    productImage.addEventListener('click',function(){
+      productImageDetail.setAttribute('src',product.image);
+      productNameDetail.innerText = product.name;
+      productPriceDetail.innerText = `$${product.price}`;
+      openProductDetailAside();
+    })
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -101,9 +115,9 @@ function renderProducts(product) {
     productInfoDivHijo.append(productPrice,productName);
     productFigure.appendChild(productFigureImage);
     cardsContainer.appendChild(productCard);
+
+
   }
 }
 
-renderProducts(productList);
-renderProducts(productList);
 renderProducts(productList);
